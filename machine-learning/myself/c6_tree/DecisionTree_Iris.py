f7c2002a1c5714b5c5441b4e53bf5c37ad57737d
dangfugui <dang@IBM704-R9H568F.cn.ibm.com>
2016-09-12 18:18:57 +0800

    updata

---
+++ machine-learning/myself/c6_tree/DecisionTree_Iris.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+'''
+Created on 2016年9月12日
+
+@author: dang
+'''
+class DecisionTree_Iris:
+    def __init__(self):
+        print 1
+        
+if __name__ == '__main__':
+    dec=DecisionTree_Iris()
